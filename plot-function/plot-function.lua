5fd8cc1bab7cd816b64cb078317016d0f3448128
Thomas Bläsius <thomas.blaesius@kit.edu>
2018-10-18 22:34:30 +0200

    making the plot-function ipelet useful

--- plot-function/plot-function.lua
+++ plot-function/plot-function.lua
@@ -1,137 +1,106 @@
 label = "Plot Function"
 
+about = [[ Very simple plotter for functions. ]]
+
+local funcStr = "x^2"
+local xMin = -3
+local xMax = 3
+local step = 0.2
+local yMin = 0
+local yMax = 9
+
+function saveInput(d)
+   funcStr = d:get("func")
+   xMin = toNumber(d:get("xMin"))   
+   xMax = toNumber(d:get("xMax"))
+   step = toNumber(d:get("step"))
+   yMin = toNumber(d:get("yMin"))
+   yMax = toNumber(d:get("yMax"))
+end
+
+function loadInput(d)
+   d:set("func", funcStr)
+   d:set("xMin", xMin)
+   d:set("xMax", xMax)
+   d:set("step", step)
+   d:set("yMin", yMin)
+   d:set("yMax", yMax)
+end
+
+function run(model)
+   local d = ipeui.Dialog(model.ui:win(), "Plot a function")
+   
+   d:add("label7", "label", {label="Use Lua syntax for your function!"}, 1, 1, 1, 6)
+   d:add("label1", "label", {label="f(x)"}, 2, 1, 1, 1)
+   d:add("func", "input", {}, 2, 2, 1, 5)
+   d:add("label2", "label", {label="x-min"}, 3, 1, 1, 1)
+   d:add("xMin", "input", {}, 3, 2, 1, 1)
+   d:add("label3", "label", {label="x-max"}, 3, 3, 1, 1)
+   d:add("xMax", "input", {}, 3, 4, 1, 1)
+   d:add("label4", "label", {label="step size"}, 3, 5, 1, 1)
+   d:add("step", "input", {}, 3, 6, 1, 1)
+   d:add("label5", "label", {label="y-min"}, 4, 1, 1, 1)
+   d:add("yMin", "input", {}, 4, 2, 1, 1)
+   d:add("label6", "label", {label="y-max"}, 4, 3, 1, 1)
+   d:add("yMax", "input", {}, 4, 4, 1, 1)
+   
+   d:addButton("ok", "&Ok", "accept")
+   d:addButton("save", "&Save", saveInput)
+   d:addButton("cancel", "&Cancel", "reject")
+   
+   loadInput(d)
+
+   if not d:execute() then return end
+   saveInput(d)
+   plot(model)
+end
+
 function plot(model)
    local p = model:page()
    local prim = p:primarySelection()
    if not prim then model.ui:explain("select a rectangle") return end
-   -- local obj = p[prim]
    local bbox = p:bbox(prim)
 
-   local func_str = model:getString("Enter a function")
-   local x_min = get_number(model, "Enter minimum value for x")
-   local x_max = get_number(model, "Enter maximum value for x")
-   local step = get_number(model, "Enter step value for x")
-   local y_min = get_number(model, "Enter minimum value for y")
-   local y_max = get_number(model, "Enter maximum value for y")
-
-   local func = _G.loadstring("return " .. func_str)
+   local func = _G.loadstring("return " .. funcStr)
   
    -- plot the function
    local path = { type="curve", closed=false }
-   local x = x_min
-   set_x(x)
+   local x = xMin
+   sestX(x)
    local y = func(x)
-   local last_point = point(x, y, x_min, x_max, y_min, y_max, bbox)
-   for x = x_min + step, x_max, step do
-      set_x(x)
+   local lastPoint = point(x, y, xMin, xMax, yMin, yMax, bbox)
+   for x = xMin + step, xMax + 0.0001, step do
+      sestX(x)
       y = func(x)
-      local point = point(x, y, x_min, x_max, y_min, y_max, bbox)
-      path[#path + 1] = { type="segment", last_point, point }
-      last_point = point
+      local point = point(x, y, xMin, xMax, yMin, yMax, bbox)
+      path[#path + 1] = { type="segment", lastPoint, point }
+      lastPoint = point
    end
 
    local plot = ipe.Path(model.attributes, { path })
    model:creation("create function plot", plot)
 end
 
-function plot2(model)
-   local p = model:page()
-   local prim = p:primarySelection()
-   if not prim then model.ui:explain("select a rectangle") return end
-   -- local obj = p[prim]
-   local bbox = p:bbox(prim)
-
-   -- local func_str = model:getString("Enter a function")
-   local x_min = 0 -- get_number(model, "Enter minimum value for x")
-   local x_max = get_number(model, "Enter maximum value for x")
-   local step = 1 -- get_number(model, "Enter step value for x")
-   local y_min = 0 -- get_number(model, "Enter minimum value for y")
-   local y_max = 0.5 -- get_number(model, "Enter maximum value for y")
-
-   -- local func = _G.loadstring("return " .. func_str)
-  
-   -- plot the function
-   local path = { type="curve", closed=false }
-   local x = x_min
-   set_x(x)
-   -- print(x)
-   local y = martins_f(x)
-   -- print(y)
-   local last_point = point(x, y, x_min, x_max, y_min, y_max, bbox)
-   for x = x_min + step, x_max, step do
-      set_x(x)
-      y = martins_f(x)
-
-      local point = point(x, y, x_min, x_max, y_min, y_max, bbox)
-      path[#path + 1] = { type="segment", last_point, point }
-      last_point = point
-      
-      local mark = ipe.Reference(model.attributes, "mark/disk(sx)", point)
-      model:creation("new point", mark)
-   end
-
-   local plot = ipe.Path(model.attributes, { path })
-   model:creation("create function plot", plot)
+function point (x, y, xMin, xMax, yMin, yMax, bbox)
+   local sX = bbox:width()/(xMax - xMin)
+   local sY = bbox:height()/(yMax - yMin)
+   local dX = bbox:left() - xMin*sX
+   local dY = bbox:bottom() - yMin*sY
+   local newX = x * sX + dX
+   local newY = y * sY + dY
+   return ipe.Vector(newX, newY)
 end
 
-
-function martins_f(t)
-   local p_0 = 0.49999
-   local res = p_0
-   for i = 1, t do
-      -- print(i)
-      res = dec(res)
-   end
-   return res
-end
-
-function dec(p)
-   return 3*p*p*(1-p) + p*p*p
-end
-
-function point (x, y, x_min, x_max, y_min, y_max, bbox)
-   local s_x = bbox:width()/(x_max - x_min)
-   local s_y = bbox:height()/(y_max - y_min)
-   local d_x = bbox:left() - x_min*s_x
-   local d_y = bbox:bottom() - y_min*s_y
-   local new_x = x * s_x + d_x
-   local new_y = y * s_y + d_y
-   return ipe.Vector(new_x, new_y)
-end
-
-function set_x (x)
+function sestX (x)
    assert(_G.loadstring("x = " .. x))()
 end
 
-function test ()
-   -- local f = assert(loadstring("function f(i) print(i); return i; end"))
-   --- f = function () i = i + 1 end
-   -- assert(loadstring("i = 0"))()
-
-   f = _G.loadstring("return x + 1") -- compile the expression to an anonymous function
-   set_x(1)
-   print(f())
-
-   -- local f = assert(loadstring("i = i + 1; return i"))
-   -- f()
-   -- print(f())   --> 1
-   -- f()
-   -- print(i)   --> 2
-end
-
-function get_number(model, text)
-   local str = model:getString(text)
+function toNumber (str)
    if not str or str:match("^%s*$)") then return end
    local k = tonumber(str)
    if not k then
-      model:warning("Enter a number between 3 and 1000!")
+      model:warning("Enter a number!")
       return
    end
    return k
 end
-
-methods = {
-   { label = "plot function", run = plot},
-   { label = "plot function2", run = plot2},
-   { label = "test", run = test},
-}
